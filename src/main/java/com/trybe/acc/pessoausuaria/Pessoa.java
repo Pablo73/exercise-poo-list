23cb6ed52143dcf4c62b16285f31982136438422
Pablo Dominguez <pasaxo@gmail.com>
2023-07-07 11:21:03 -0300

    Requisito 3

--- src/main/java/com/trybe/acc/pessoausuaria/Pessoa.java
+++ src/main/java/com/trybe/acc/pessoausuaria/Pessoa.java
@@ -1,5 +1,12 @@
 package com.trybe.acc.pessoausuaria;
 
 public class Pessoa {
+  protected String nome;
+  protected String sobrenome;
+
+  Pessoa(String nome, String sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
 
 }
--- src/main/java/com/trybe/acc/pessoausuaria/PessoaUsuaria.java
+++ src/main/java/com/trybe/acc/pessoausuaria/PessoaUsuaria.java
@@ -2,15 +2,26 @@ package com.trybe.acc.pessoausuaria;
 
 public class PessoaUsuaria extends Pessoa {
 
-  public PessoaUsuaria() {
-
+  public PessoaUsuaria(String nome, String sobrenome) {
+    super(nome, sobrenome);
   }
 
   /**
    * Método para pegar o nome e sobrenome do usuário.
-   *
+   * 
+   * @return o nome de usuário concatenado com o sobrenome, ou uma mensagem de "Pessoa usuária
+   *         inválida" se o nome ou sobrenome forem inválidos
    */
   public String getPessoaUsuaria() {
-    return null;
+    if (nome == null || nome.isEmpty() || sobrenome == null || sobrenome.isEmpty()) {
+      return "Pessoa usuária inválida";
+    } else {
+      return nome + "." + sobrenome;
+    }
+  }
+
+  public static void main(String[] args) {
+    PessoaUsuaria pessoaUsuaria = new PessoaUsuaria(null, "wayne");
+    System.out.println(pessoaUsuaria.getPessoaUsuaria());
   }
 }
